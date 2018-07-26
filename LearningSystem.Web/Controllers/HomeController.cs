33362f7014ad0668177fbdad8482d2d4d20caf74
Slav Tomov <stomow@gmail.com>
2018-07-26 12:14:28 +0300

    Fix browser back button bug from search.

--- LearningSystem.Web/Controllers/HomeController.cs
+++ LearningSystem.Web/Controllers/HomeController.cs
@@ -11,6 +11,7 @@
     using System.Diagnostics;
     using System.Threading.Tasks;
 
+    [IgnoreAntiforgeryToken]
     public class HomeController : Controller
     {
         private readonly ICourseService courses;
